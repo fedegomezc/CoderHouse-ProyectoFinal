e9a2eb9427c39245704c29ad4cf245c65e5822fc
Federico Gomez Conde <fedeic8@gmail.com>
2022-09-23 11:43:38 -0300

    add button to remove items from cart

---
+++ js/products.js
@@ -0,0 +1,66 @@
+const cafes = [
+    {
+        id: 1,
+        name: 'AUTENTICO | BLENDS | 250G',   
+        price: 990,
+        category: 'BLEND-BRASIL',
+        description: 'Café Autentico Blend. Notas: Aroma dulce a almendra tostada y chocolate',
+        image: '/Images/AUTENTICO-BLENDS.jpg'
+    },
+    {
+        id: 2,
+        name: 'CHANGO | BLENDS | 250G',   
+        price: 1290,
+        category: 'BLEND-M',
+        description: 'Café Chango Blend. Notas: Frutos Secos & Maduros',
+        image: '/Images/CHANGO-BLENDS.jpg'
+    },
+    {
+        id: 3,
+        name: 'CHULLO | BLENDS | 250G',
+        price: 1290,
+        category: 'BLEND-M',
+        description: 'Cafe Chullo Blend. Notas: Cítricas & Cacao',
+        image: '/Images/CHULLO-BLENDS.jpg'
+    },
+    {
+        id: 4,
+        name: 'CREMA | BLENDS | 250G',
+        price: 990,
+        category: 'BLEND-BRASIL',
+        description: 'Café Crema Blend. Notas: Aroma intenso a Chocolate',
+        image: '/Images/CREMA-BLENDS.jpg'
+    },
+    {
+        id: 5,
+        name: 'SANTANDER | COLOMBIA | 250G',
+        price: 2650,  
+        category: 'COLOMBIA-M',
+        description: 'Café Santander, Colombia. Notas: azucar morena, miel y cacao',     
+        image: '/Images/SANTANDER-COLOMBIA.jpg'
+    },
+    {
+        id: 6,
+        name: 'FRESCO | BLENDS | 250G',
+        price: 990,
+        category: 'BLEND-BRASIL',
+        description: 'Café Fresco Blend. Notas: Aroma dulce a almendra tostada. Acidez dulce a naranja',       
+        image: '/Images/FRESCO-BLENDS.jpg'
+    },
+    {
+        id: 7,
+        name: 'MINAS GERAIS | BRASIL | 250G',
+        price: 1803,
+        category: 'BRASIL-M',    
+        description: 'Café Minas Gerais, Brasil. Notas: azucar mascabo, fruta madura y chocolate',
+        image: '/Images/MINA-GERAIS-BRASIL.jpg'
+    },
+    {
+        id: 8,
+        name: 'COBAN | GUATEMALA | 250G',
+        price: 2300,
+        category: 'GUATEMALA-ORGANICO',   
+        description: 'Café Coban, Guatemala. Notas: dulce y sabroso con acidez tartárica, sabores praliné, tofee, manzana, cítricos',
+        image: '/Images/COBAN-GUATEMALA.jpg'
+    }
+];
--- js/script.js
+++ js/script.js
@@ -1,75 +1,8 @@
-// Products list
+// Products cart
 const cart = [];
-const cafes = [
-    {
-        id: 1,
-        name: 'AUTENTICO | BLENDS | 250G',   
-        price: 990,
-        category: 'BLEND-BRASIL',
-        description: 'Café Autentico Blend. Notas: Aroma dulce a almendra tostada y chocolate',
-        image: '/Images/AUTENTICO-BLENDS.jpg'
-    },
-    {
-        id: 2,
-        name: 'CHANGO | BLENDS | 250G',   
-        price: 1290,
-        category: 'BLEND-M',
-        description: 'Café Chango Blend. Notas: Frutos Secos & Maduros',
-        image: '/Images/CHANGO-BLENDS.jpg'
-    },
-    {
-        id: 3,
-        name: 'CHULLO | BLENDS | 250G',
-        price: 1290,
-        category: 'BLEND-M',
-        description: 'Cafe Chullo Blend. Notas: Cítricas & Cacao',
-        image: '/Images/CHULLO-BLENDS.jpg'
-    },
-    {
-        id: 4,
-        name: 'CREMA | BLENDS | 250G',
-        price: 990,
-        category: 'BLEND-BRASIL',
-        description: 'Café Crema Blend. Notas: Aroma intenso a Chocolate',
-        image: '/Images/CREMA-BLENDS.jpg'
-    },
-    {
-        id: 5,
-        name: 'SANTANDER | COLOMBIA | 250G',
-        price: 2650,  
-        category: 'COLOMBIA-M',
-        description: 'Café Santander, Colombia. Notas: azucar morena, miel y cacao',     
-        image: '/Images/SANTANDER-COLOMBIA.jpg'
-    },
-    {
-        id: 6,
-        name: 'FRESCO | BLENDS | 250G',
-        price: 990,
-        category: 'BLEND-BRASIL',
-        description: 'Café Fresco Blend. Notas: Aroma dulce a almendra tostada. Acidez dulce a naranja',       
-        image: '/Images/FRESCO-BLENDS.jpg'
-    },
-    {
-        id: 7,
-        name: 'MINAS GERAIS | BRASIL | 250G',
-        price: 1803,
-        category: 'BRASIL-M',    
-        description: 'Café Minas Gerais, Brasil. Notas: azucar mascabo, fruta madura y chocolate',
-        image: '/Images/MINA-GERAIS-BRASIL.jpg'
-    },
-    {
-        id: 8,
-        name: 'COBAN | GUATEMALA | 250G',
-        price: 2300,
-        category: 'GUATEMALA-ORGANICO',   
-        description: 'Café Coban, Guatemala. Notas: dulce y sabroso con acidez tartárica, sabores praliné, tofee, manzana, cítricos',
-        image: '/Images/COBAN-GUATEMALA.jpg'
-    }
-];
-
-
 
 // dynamic loading of products
+// Products data is in 'products.js'
 // If another product is added in 'cafes' array, the page is automatically updated
 const loadProducts = (cafes) =>
 {
@@ -163,6 +96,7 @@ const updateCart = (cart) =>
                 <h4>${product.name}</h4>
                 <h4>$ ${product.price}</h4>
                 <h4>Cantidad: ${product.quantity}</h4>
+                <button class="buttonX" id="${product.id}">X</button>
             </div>
         `;
         // sum total price
@@ -175,6 +109,36 @@ const updateCart = (cart) =>
     `;
     // add node to cartContainer
     cartContainer.appendChild(div);
+    // xButton function
+    xButtonEvent()
+}
+
+// Function to remove an element from array by ID
+const removeItem = (array, id) => 
+{
+    // Find the index of the object with the id in the array 
+    // use '==' because 'obj.id' it's number and 'id' it's string
+    const objIndex = array.findIndex((obj) => obj.id == id);
+    // remove the object from the array
+    array.splice(objIndex, 1);
+
+    return array; 
+}
+
+const removeItemFromCart = (id) => 
+{
+    let newCart = removeItem(cart, id)
+    updateCart(newCart)
+}
+// x buttons function - removeItem from cart
+const xButtonEvent = () => 
+{
+    // Select all buttons
+    let buttonx = document.querySelectorAll('.buttonX');
+    // Loop buttons and remove from cart list when the 'click' event occurs
+    for (const button of buttonx) {
+        button.addEventListener('click', ()=> removeItemFromCart(button.id))
+    }
 }
 
 loadProducts(cafes);
