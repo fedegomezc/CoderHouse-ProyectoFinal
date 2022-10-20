414fa8b83b1f6b6caf1fc0aca1fce7d2b3d1b1f0
Federico Gomez Conde <fedeic8@gmail.com>
2022-10-20 18:34:16 -0300

    cart upgrade

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-let cafes = [];
 let cart = [];
+let cafes = [];
 let container = document.querySelector('#container');
-let cartContainer = document.querySelector('#cart');
 let inputFilter = document.querySelector("#productFilter");
+
+// Return cart values from LocalStorage
+const valuesFromLS = () => {
+    cart = []
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i)
+        value = JSON.parse(localStorage.getItem(key))
+        cart.push(value)
+    }
+}
+// If localStorage has items return values
+const returnCartValues = () => localStorage.length && valuesFromLS()
+
+returnCartValues();
 
 // If another product is added in 'cafes' array, the page is automatically updated
 const loadProducts = (cafes) =>
@@ -100,110 +113,6 @@ const addToCart = (id) =>
     // else add the product to cart
     let found = cart.find(element => element.id == id);
             found ? addQuantity(found) : addNewItem()
-
-    updateCart(cart);
-}
-
-// every time a product is added the cart is updated
-const updateCart = (cart) =>
-{
-    let container = document.getElementById("cartContainer");
-    // if the container exist it is removed
-    container && container.parentNode.removeChild(container);
-    // create element for new products added in cartContainer 
-    let div = document.createElement('div');
-    div.setAttribute('id','cartContainer');
-    div.innerHTML += ` <h2>Carrito de compras</h2>`;
-    // variable to save total price of products
-    let total = 0;
-    for (const product of cart)
-    {
-        let {id, name, price, quantity} = product
-
-        div.innerHTML += `
-            <div class="cart-item">
-                <h4>${name}</h4>
-                <h4>Precio: $ ${price}</h4>
-                <h4>Cantidad: ${quantity} 
-                <button class="buttonAdd" id="${id}">+</button>
-                <button class="buttonRemove" id="${id}">-</button></h4>
-                <h4>Subtotal: $ ${price * quantity}</h4>
-                <button class="buttonX" id="${id}">Eliminar</button>
-            </div>
-        `;
-        total += price * quantity
-    }
-    div.innerHTML += `
-        <h2>Total = $
-        ${total}</h2>
-    `;
-    cartContainer.appendChild(div);
-    
-    xButtonEvent()
-    addQuantityButton()
-    subtractQuantityButton()
-}
-
-// Function to remove an element from array by ID
-const removeItem = (array, id) => 
-{
-    const objIndex = array.findIndex((obj) => obj.id == id);
-    array.splice(objIndex, 1);
-    return array; 
-}
-
-const removeItemFromCart = (id) => 
-{
-    let newCart = removeItem(cart, id)
-    localStorage.removeItem(`${id}`)
-    updateCart(newCart)
-}
-// x buttons Event - removeItem from cart
-const xButtonEvent = () => 
-{
-    let buttonx = document.querySelectorAll('.buttonX');
-    for (const button of buttonx) {
-        button.addEventListener('click', ()=> removeItemFromCart(button.id))
-    }
-}
-
-
-const addQuantity = (id) =>
-{
-    const addOneItem = (obj) => {
-        objID.quantity++;
-        localStorage.removeItem(`${id}`);
-        localStorage.setItem(`${obj.id}`, JSON.stringify(obj));
-        updateCart(cart); 
-    }
-    const objID = cart.find((obj) => obj.id == id);
-        objID.quantity < 10 && addOneItem(objID);  
-}
-const addQuantityButton = () =>
-{
-    let buttonAdd = document.querySelectorAll('.buttonAdd');
-    for (const button of buttonAdd) {
-        button.addEventListener('click', ()=> addQuantity(button.id))
-    }
-}
-
-const subtractQuantity = (id) =>
-{
-    const removeOneItem = (obj) => {
-        objID.quantity--;
-        localStorage.removeItem(`${id}`);
-        localStorage.setItem(`${obj.id}`, JSON.stringify(obj));
-        updateCart(cart);
-    }
-    const objID = cart.find((obj) => obj.id == id);
-        objID.quantity > 1 && removeOneItem(objID);       
-}
-const subtractQuantityButton = () =>
-{
-    let buttonRemove = document.querySelectorAll('.buttonRemove');
-    for (const button of buttonRemove) {
-        button.addEventListener('click', ()=> subtractQuantity(button.id))
-    }
 }
 
 const searchProd = () => 
@@ -221,20 +130,3 @@ const searchProd = () =>
     }
 }
 inputFilter.addEventListener("input", searchProd)
-
-
-// Return cart values from LocalStorage
-const valuesFromLS = () => {
-    cart = []
-    for (let i = 0; i < localStorage.length; i++) {
-        let key = localStorage.key(i)
-        value = JSON.parse(localStorage.getItem(key))
-        cart.push(value)
-    }
-    updateCart(cart)
-}
-// If localStorage has items return values
-const returnCartValues = () => localStorage.length && valuesFromLS()
-
-// loadProducts(cafes);
-returnCartValues()
